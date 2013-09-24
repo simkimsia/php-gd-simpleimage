7928028773eb2719b3b54888d6416e84971ce945
kimsia <kimcity@gmail.com>
2013-09-24 16:33:19 +0800

    add a generate square thumbnail method

--- SimpleImage.php
+++ SimpleImage.php
@@ -62,8 +62,7 @@ class SimpleImage {
 		imagedestroy($source);
 
 		return new SimpleImage($path);
-		
-	}	
+	}
 
 	public function generateThumbnail ($thumbnail_image_path) {
 		// load source gd
@@ -80,30 +79,79 @@ class SimpleImage {
 
 		// if source smaller than thumb just use source
 		if ($this->sourceImageWidth <= $this->thumbnailMaxWidth && $this->sourceImageHeight <= $this->thumbnailMaxHeight) {
-        	$thumbnail_image_width = $this->sourceImageWidth;
-        	$thumbnail_image_height = $this->sourceImageHeight;
-    	} elseif ($thumbnail_aspect_ratio > $source_aspect_ratio) {
-    		// if thumb aspect ratio larger than source aspect ratio
-    		// then we maintain maxheight and take a fraction of the width
-        	$thumbnail_image_width = (int) ($this->thumbnailMaxHeight * $source_aspect_ratio);
-        	$thumbnail_image_height = $this->thumbnailMaxHeight;
-    	} else {
-    		// if thumb aspect ratio less than source aspect ratio
-    		// then we maintain maxwidth and take a fraction of the height
-        	$thumbnail_image_width = $this->thumbnailMaxWidth;
-        	$thumbnail_image_height = (int) ($this->thumbnailMaxWidth / $source_aspect_ratio);
-    	}
+			$thumbnail_image_width = $this->sourceImageWidth;
+			$thumbnail_image_height = $this->sourceImageHeight;
+		} elseif ($thumbnail_aspect_ratio > $source_aspect_ratio) {
+			// if thumb aspect ratio larger than source aspect ratio
+			// then we maintain maxheight and take a fraction of the width
+			$thumbnail_image_width = (int) ($this->thumbnailMaxHeight * $source_aspect_ratio);
+			$thumbnail_image_height = $this->thumbnailMaxHeight;
+		} else {
+			// if thumb aspect ratio less than source aspect ratio
+			// then we maintain maxwidth and take a fraction of the height
+			$thumbnail_image_width = $this->thumbnailMaxWidth;
+			$thumbnail_image_height = (int) ($this->thumbnailMaxWidth / $source_aspect_ratio);
+		}
 
-    	// create the gd image of thumb based on new thumb width, height
-    	$thumbnail_gd_image = imagecreatetruecolor($thumbnail_image_width, $thumbnail_image_height);
+		// create the gd image of thumb based on new thumb width, height
+		$thumbnail_gd_image = imagecreatetruecolor($thumbnail_image_width, $thumbnail_image_height);
 
-    	// copy from source gd and fill inside thumb gd
-    	imagecopyresampled($thumbnail_gd_image, $this->sourceGdImage, 0, 0, 0, 0, $thumbnail_image_width, $thumbnail_image_height, $this->sourceImageWidth, $this->sourceImageHeight);
-    	// create the image file from the gd 
-    	imagejpeg($thumbnail_gd_image, $thumbnail_image_path, 90);
+		// copy from source gd and fill inside thumb gd
+		imagecopyresampled($thumbnail_gd_image, $this->sourceGdImage, 0, 0, 0, 0, $thumbnail_image_width, $thumbnail_image_height, $this->sourceImageWidth, $this->sourceImageHeight);
+		// create the image file from the gd 
+		imagejpeg($thumbnail_gd_image, $thumbnail_image_path, 90);
 
-    	imagedestroy($thumbnail_gd_image);
-    	return true;
+		imagedestroy($thumbnail_gd_image);
+		return true;
+	}
+
+/**
+ * take a given image and produce a square thumbnail while filling the background with white color
+ */
+	public function generateSquareThumbnail ($thumbnail_image_path) {
+		// load source gd
+		$this->loadSourceIntoGD();
+
+		if ($this->sourceGdImage === false) {
+			return false;
+		}
+
+		// get source aspect ratio
+		// and thumbnail aspect ratio
+		$source_aspect_ratio	= $this->getAspectRatio();
+		$thumbnail_aspect_ratio	= $this->getThumbNailAspectRatio();
+
+		// we will not do for smaller than thumbnail max width and max height
+		if ($this->sourceImageWidth <= $this->thumbnailMaxWidth && $this->sourceImageHeight <= $this->thumbnailMaxHeight) {
+			return false;
+		} elseif ($thumbnail_aspect_ratio > $source_aspect_ratio) {
+			// if thumb aspect ratio larger than source aspect ratio
+			// then we maintain maxheight and take a fraction of the width
+			$thumbnail_image_width = (int) ($this->thumbnailMaxHeight * $source_aspect_ratio);
+			$thumbnail_image_height = $this->thumbnailMaxHeight;
+		} else {
+			// if thumb aspect ratio less than source aspect ratio
+			// then we maintain maxwidth and take a fraction of the height
+			$thumbnail_image_width = $this->thumbnailMaxWidth;
+			$thumbnail_image_height = (int) ($this->thumbnailMaxWidth / $source_aspect_ratio);
+		}
+
+
+		// create a white background image first of max width and max height
+		$background = imagecreatetruecolor($this->thumbnailMaxWidth,$this->thumbnailMaxHeight);
+		$whiteBackground = imagecolorallocate($background, 255, 255, 255);
+		imagefill($background,0,0,$whiteBackground);
+
+		// // create the gd image of thumb based on new thumb width, height
+		// $thumbnail_gd_image = imagecreatetruecolor($thumbnail_image_width, $thumbnail_image_height);
+
+		// copy from source gd and fill inside thumb gd
+		imagecopyresampled($background, $this->sourceGdImage, ($this->thumbnailMaxWidth-$thumbnail_image_width)/2, ($this->thumbnailMaxHeight-$thumbnail_image_height)/2, 0, 0, $thumbnail_image_width, $thumbnail_image_height, $this->sourceImageWidth, $this->sourceImageHeight);
+		// create the image file from the gd 
+		imagejpeg($background, $thumbnail_image_path, 90);
+
+		imagedestroy($thumbnail_gd_image);
+		return true;
 	}
 
 	public function getAspectRatio() {
